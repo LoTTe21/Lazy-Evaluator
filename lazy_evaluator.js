aeff8701a76ce24d51e224430de5f202a9ea97e9
Yujian <yujian_fu@outlook.com>
2020-04-12 14:42:17 +0800

    add all funcitons in textbook

--- lazy_evaluator.js
+++ lazy_evaluator.js
@@ -523,10 +523,45 @@ function evaluate(stmt, env) {
           ? eval_return_statement(stmt, env)
         : is_application(stmt)
           ? apply(evaluate(operator(stmt), env),
-                  list_of_values(operands(stmt), env))
+                  list_of_values(operands(stmt),
+                  env))
         : error(stmt, "Unknown statement type in evaluate: ");
 }
 
+
+function actual_value(exp, env)
+{
+	return force_it(evaluate(exp, env));
+}
+
+
+function evaluate(stmt, env) {
+   return is_self_evaluating(stmt)
+          ?  stmt
+        : is_name(stmt)
+          ? lookup_name_value(name_of_name(stmt), env)
+        : is_constant_declaration(stmt)
+          ? eval_constant_declaration(stmt, env)
+        : is_variable_declaration(stmt)
+          ? eval_variable_declaration(stmt, env)
+        : is_assignment(stmt)
+          ? eval_assignment(stmt, env)
+        : is_conditional_expression(stmt)
+          ? eval_conditional_expression(stmt, env)
+        : is_function_definition(stmt)
+          ? eval_function_definition(stmt, env)
+        : is_sequence(stmt)
+          ? eval_sequence(sequence_statements(stmt), env)
+        : is_block(stmt)
+          ? eval_block(stmt, env)
+        : is_return_statement(stmt)
+          ? eval_return_statement(stmt, env)
+        : is_application(stmt)
+          ? apply(actual_value(operator(stmt), env),
+                  operands(stmt),
+                  env)
+        : error(stmt, "Unknown statement type in evaluate: ");
+}
 
 //apply a function to arguments
 // there are two kinds of function: primitive and compound function
@@ -556,8 +591,24 @@ function apply(fun, args) {
    } else {
       error(fun, "Unknown function type in apply");
    }
-}
+}si
 
+function apply(fun, args)
+{
+	if (is_primitive_function(fun))
+	{
+		return apply_primitive_function(
+			fun,
+			list_of_arg_values(args, env));
+	}
+	else if (is_compound_function(fun)) 
+	{
+		const result = 
+		evaluate(function_body(fun),
+			extend_environment(function_parameters(fun),
+				))
+	}
+}
 
 
 /**********************************************************************/
